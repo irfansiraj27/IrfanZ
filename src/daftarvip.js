e2a74651e72a116c02d39beb3096a11e7655774a
irfansiraj27 <74446537+irfansiraj27@users.noreply.github.com>
2021-01-15 19:18:45 +0800

    Add files via upload

---
+++ src/daftarvip.js
@@ -0,0 +1,15 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*HARGA DAFTAR VIP :*
+-Rp. 5K > Akses Fitur ViP
+-Rp. 10K > Fitur VIP + Masukin Bot KeGrup Kalian!
+
+*JIKA INGIN DAFTAR VIP :*
+*Chat Owner BOT :*
+_wa.me/62895321033713 atau ketik *${prefix}owner*
+
+*NOTE*
+
+}
+exports.daftarvip = daftarvip
